4283727a6d120e2d3c0802efecc6cce6d81afc1d
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-10 05:49:09 -0500

    update Program.cs with valid data

--- PeasyDotNetFrameworkSample/Program.cs
+++ PeasyDotNetFrameworkSample/Program.cs
@@ -2,7 +2,6 @@
 using PeasyDotNetFrameworkSample.BusinessLogic.DTO;
 using PeasyDotNetFrameworkSample.BusinessLogic.Services;
 using System.Diagnostics;
-using System.Linq;
 
 namespace PeasyDotNetFrameworkSample
 {
@@ -11,16 +10,14 @@ namespace PeasyDotNetFrameworkSample
         static void Main(string[] args)
         {
             var service = new PersonService(new PersonMockDataProxy());
-            var newPerson = new Person() { Name = "Fred Jones", City = "Nowhere" };
+            var newPerson = new Person() { Name = "Freida Jones", City = "Madison" };
             var insertResult = service.InsertCommand(newPerson).Execute();
             if (insertResult.Success)
             {
-                Debug.WriteLine(insertResult.Value.ID.ToString());
+                Debug.WriteLine(insertResult.Value.ID.ToString()); // prints the id value assigned via PersonMockDataProxy.Insert
             }
             else
             {
-                // This line will execute and print 'Name cannot be fred jones' and 'Nowhere is not a city'
-                // Note that insertResult.Value will be NULL as PersonMockDataProxy.Insert did not execute due to failed rule
                 foreach (var error in insertResult.Errors)
                     Debug.WriteLine(error);
             }
