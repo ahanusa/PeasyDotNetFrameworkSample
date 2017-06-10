962e130c8a5091c2cef64e2d0e372fc9661488e4
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-10 05:44:34 -0500

    add ValidCityRule

---
+++ PeasyDotNetFrameworkSample/Rules/ValidCityRule.cs
@@ -0,0 +1,22 @@
+﻿using Peasy;
+
+namespace PeasyDotNetFrameworkSample.Rules
+{
+    public class ValidCityRule : RuleBase
+    {
+        private string _city;
+
+        public ValidCityRule(string city)
+        {
+            _city = city;
+        }
+
+        protected override void OnValidate()
+        {
+            if (_city == "Nowhere")
+            {
+                Invalidate("Nowhere is not a city");
+            }
+        }
+    }
+}
