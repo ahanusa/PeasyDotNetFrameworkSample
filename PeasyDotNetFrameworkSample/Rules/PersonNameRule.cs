560e21571923fb6625c37a21ae02cd846959337d
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-10 05:40:25 -0500

    add PersonNameRule

---
+++ PeasyDotNetFrameworkSample/Rules/PersonNameRule.cs
@@ -0,0 +1,22 @@
+﻿using Peasy;
+
+namespace PeasyDotNetFrameworkSample.Rules
+{
+    public class PersonNameRule : RuleBase
+    {
+        private string _name;
+
+        public PersonNameRule(string name)
+        {
+            _name = name;
+        }
+
+        protected override void OnValidate()
+        {
+            if (_name == "Fred Jones")
+            {
+                Invalidate("Name cannot be fred jones");
+            }
+        }
+    }
+}
