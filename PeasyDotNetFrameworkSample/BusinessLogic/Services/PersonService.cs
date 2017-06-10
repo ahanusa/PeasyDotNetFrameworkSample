811e5b788d4e11bc730e828b0b6e9dc90fe66099
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-10 05:33:27 -0500

    add PersonService

---
+++ PeasyDotNetFrameworkSample/BusinessLogic/Services/PersonService.cs
@@ -0,0 +1,12 @@
+﻿using Peasy;
+using PeasyDotNetFrameworkSample.BusinessLogic.DTO;
+
+namespace PeasyDotNetFrameworkSample.BusinessLogic.Services
+{
+    public class PersonService : ServiceBase<Person, int>
+    {
+        public PersonService(IDataProxy<Person, int> dataProxy) : base(dataProxy)
+        {
+        }
+    }
+}
