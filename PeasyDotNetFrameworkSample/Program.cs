92c21843bc202c817456b629cf2a17d377297ab4
Aaron Hanusa <aaron_hanusa@hotmail.com>
2017-06-10 05:43:10 -0500

    update Program.cs

--- PeasyDotNetFrameworkSample/Program.cs
+++ PeasyDotNetFrameworkSample/Program.cs
@@ -2,6 +2,7 @@
 using PeasyDotNetFrameworkSample.BusinessLogic.DTO;
 using PeasyDotNetFrameworkSample.BusinessLogic.Services;
 using System.Diagnostics;
+using System.Linq;
 
 namespace PeasyDotNetFrameworkSample
 {
@@ -10,18 +11,17 @@ namespace PeasyDotNetFrameworkSample
         static void Main(string[] args)
         {
             var service = new PersonService(new PersonMockDataProxy());
-            var getResult = service.GetAllCommand().Execute();
-            if (getResult.Success)
-            {
-                foreach (var person in getResult.Value)
-                    Debug.WriteLine(person.Name);  // prints each person's name retrieved from PersonMockDataProxy.GetAll
-            }
-
-            var newPerson = new Person() { Name = "Freed Jones", City = "Madison" };
+            var newPerson = new Person() { Name = "Fred Jones", City = "Madison" };
             var insertResult = service.InsertCommand(newPerson).Execute();
             if (insertResult.Success)
             {
-                Debug.WriteLine(insertResult.Value.ID.ToString()); // prints the id value assigned via PersonMockDataProxy.Insert
+                Debug.WriteLine(insertResult.Value.ID.ToString());
+            }
+            else
+            {
+                // This line will execute and print 'Name cannot be fred jones' 
+                // Note that insertResult.Value will be NULL as PersonMockDataProxy.Insert did not execute due to failed rule
+                Debug.WriteLine(insertResult.Errors.First());
             }
         }
     }
